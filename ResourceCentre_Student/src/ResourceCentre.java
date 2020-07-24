9f539e8af9ccc24da7755d0a25af2de695b62e89
19017967 <19017967@DESKTOP-MD82Q4G>
2020-07-24 13:51:34 +0800

    add

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -173,11 +173,20 @@ public class ResourceCentre {
 	public static Chromebook inputChromebook() {	
 		Chromebook cb =null;
 		// write your code here
-		return cb;
+		//Lauranda
+		String tag = Helper.readString("Enter asset tag > ");
+		String description = Helper.readString("Enter description > ");
+		String OS = Helper.readString ("Enter OS > ");
+
+
+	
 		
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
+		//Lauranda
+		chromebookList.add(cb);
+		System.out.println("Chromebook added");
 	}
 	
 	//================================= Option 3 Loan =================================
